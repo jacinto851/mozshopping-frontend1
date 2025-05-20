4fcb3084b884a2147a6dd7cf39c8be80e54011a3
jacinto851 <jacintodasilvadarkmoz@gmail.com>
2025-05-20 23:57:26 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+export default function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">Moz Shopping Online</h1>
+      <p className="mt-4 text-gray-700">Compre diamantes, gift cards, TV, e mais!</p>
+    </div>
+  );
+}
